06eb7fe2827d07a813a493df1c753a74504d3cd6
Zeyu-Li <zeyu.li23@gmail.com>
2021-11-06 11:12:39 -0600

    added timer

--- Scripts/Timer.gd
+++ Scripts/Timer.gd
@@ -1,6 +1,7 @@
 extends Label
 
-var DisplayValue = 0
+export(int) var timer_value = 0
+onready var start_time = OS.get_ticks_msec()
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -8,10 +9,14 @@ var prefix = "Timer: "
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	self.text = prefix + str(DisplayValue)
+#func _ready():
+#	self.text = prefix + str(DisplayValue)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	var time = (OS.get_ticks_msec() - start_time) / 10
+	timer_value = time
+	var milli = time / 10
+	var seconds = (milli / 10) % 100
+	var minutes = (seconds / 60) % 60
+	self.text = prefix + "%0*d" % [2, minutes] + ':' + "%0*d" % [2, seconds] + ':' + str(milli)[-1]
+	

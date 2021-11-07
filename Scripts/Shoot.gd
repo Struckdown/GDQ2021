b75c64b77e8d21df75201ce498c5c080e2642696
Zeyu-Li <zeyu.li23@gmail.com>
2021-11-07 09:47:39 -0700

    drone explodes

--- Scripts/Shoot.gd
+++ Scripts/Shoot.gd
@@ -2,7 +2,7 @@ extends Sprite
 
 
 var missile = preload("res://Projectiles/Missile.tscn")
-export(float) var max_distance = 450
+export(float) var max_distance = 500
 
 
 # Called when the node enters the scene tree for the first time.
@@ -21,7 +21,7 @@ func _on_Shoot_timeout():
 	var dodo_position = get_parent().get_parent().get_node("Dodo").global_position
 	# if further than some dist do not shoot
 	if global_position.distance_to(dodo_position) < max_distance:
-		print(global_position.distance_to(dodo_position))
+#		print(global_position.distance_to(dodo_position))
 		var m = missile.instance()
 		get_viewport().add_child(m)
 		m.speed *= 1

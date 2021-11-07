69c1c78eb8bee207f0a32bfea9d775754ed52f62
FifteenPeruvianHuevos <wuchihhung0905@gmail.com>
2021-11-07 06:48:45 -0700

    Change to BGM 2
    I just modified it so we listen to BGM 2
    and see how it feels.
    
    Could always switch back to BGM 1
    by commenting the BGM2 lines in MainLevel.gd
    
    Looping should be taken care of, reimported and play-tested.

--- MainLevel.gd
+++ MainLevel.gd
@@ -5,7 +5,8 @@ var playingSong = "intro"
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	CameraManager.cameraRef = $Dodo/Camera2D
-	BGM.requestSong("res://BGM/GDQ - 2021 - BGM Phase 1 - Intro.mp3")
+	#BGM.requestSong("res://BGM/GDQ - 2021 - BGM Phase 1 - Intro.mp3")
+	BGM.requestSong("res://BGM/GDQ - 2021 - BGM Phase 2 - Intro.mp3")
 	var err = BGM.connect("finished", self, "_on_BGM_finished")
 	if err:
 		print("Error:", err)
@@ -18,7 +19,8 @@ func _ready():
 func _on_BGM_finished():
 	match playingSong:
 		"intro":
-			BGM.requestSong("res://BGM/GDQ - 2021 - BGM Phase 1 - Main 1.mp3", 0, 0)
+			#BGM.requestSong("res://BGM/GDQ - 2021 - BGM Phase 1 - Main 1.mp3", 0, 0)
+			BGM.requestSong("res://BGM/GDQ - 2021 - BGM Phase 2 - Main.mp3", 0, 0)
 			playingSong = "main"
 		"main":
 			pass	# keep playing the same song
